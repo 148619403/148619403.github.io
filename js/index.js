33efcf80186e170ca0e7195fda2c43ac84272c1e
148619403 <136911752+148619403@users.noreply.github.com>
2023-06-22 14:24:36 +0000

    new index envlope

--- js/index.js
+++ js/index.js
@@ -1,30 +1,26 @@
-//DOM load event
-window.addEventListener("DOMContentLoaded", () => {
-
-    const cube = document.querySelector(".cube"),
-    imageButtons = document.querySelector(".image-buttons");
-    let cubeImageClass = cube.classList[1];
-  
-    //Add click event listener to image buttons container
-    imageButtons.addEventListener("click", e => {
-  
-      //Get node type and class value of clicked element
-      const targetNode = e.target.nodeName,
-      targetClass = e.target.className;
-  
-      //Check if image input has been clicked and isn't the currently shown image
-      if (targetNode === "INPUT" && targetClass !== cubeImageClass) {
-  
-        console.log(`Show Image: ${targetClass.charAt(11)}`);
-  
-        //Replace previous cube image class with new class
-        cube.classList.replace(cubeImageClass, targetClass);
-  
-        //Update cube image class variable with new class
-        cubeImageClass = targetClass;
-  
-      }
-  
+$( document ).ready(function() {
+    
+    var envelope = $('#envelope');
+    var btn_open = $("#open");
+    var btn_reset = $("#reset");
+    
+    envelope.click( function() {
+        open();
     });
-  
-  });
+    btn_open.click( function() {
+        open();
+    });
+    btn_reset.click( function() {
+        close();
+    });
+
+    function open() {
+        envelope.addClass("open")
+           .removeClass("close");
+    }
+    function close() {
+        envelope.addClass("close")
+           .removeClass("open");
+    }
+   
+});
